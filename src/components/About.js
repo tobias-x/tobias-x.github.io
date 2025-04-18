78d77b45985936332c78f86573ee0188fe41eeb9
Tobias Franks <tobias.franks@icloud.com>
2025-04-18 13:03:25 +1000

    censored

--- src/components/About.js
+++ src/components/About.js
@@ -66,7 +66,7 @@ const About = () => {
         >
           {displayText}
         </pre>
-        <p className="mt-4 text-lg text-center">I'm a ~27 year old living in Australia who likes to build cool shit.</p>
+        <p className="mt-4 text-lg text-center">I'm a ~27 year old living in Australia who likes to write software and build stuff.</p>
       </div>
     </section>
   );
